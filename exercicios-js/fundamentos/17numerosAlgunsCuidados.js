a590a1b1ddd09e2ba204601388878583438e9414
Danilo Alves <danilovieiraalves@hotmail.com>
2019-04-11 20:03:42 -0300

    Arquivos relacionados ao meus estudos em web development.

---
+++ exercicios-js/fundamentos/17numerosAlgunsCuidados.js
@@ -0,0 +1,16 @@
+//Retorna infinito
+console.log(7/0);
+console.log('10'/2);
+console.log('3' + 2); //concatena
+console.log('3' - 2); //subtrai
+console.log('10.2'/3)
+//Retorna NaN
+console.log("Show!"*2);
+//Retorna um valor impreciso
+console.log(0.1+0.7);
+//Não funciona
+//console.log(10.toString());
+console.log(toString(10));
+//Funciona
+console.log((10).toString());
+console.log((10.342).toFixed(2));
