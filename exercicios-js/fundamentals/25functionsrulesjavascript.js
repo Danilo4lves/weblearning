b7f0eb9582adb8844fcee0ea4fc4f2754f418811
Danilo Alves <danilovieiraalves@hotmail.com>
2019-04-12 09:43:51 -0300

    Functions rules most of javascript structures

---
+++ exercicios-js/fundamentals/25functionsrulesjavascript.js
@@ -0,0 +1,13 @@
+console.log(typeof Object)
+/*
+* The Object, which is also a very important structure within
+* javascript, has as it's type function.
+*/
+
+class Test{}
+console.log(typeof Test)
+/*
+* Test, which is a class I just created, returns it's type as
+* function. So, it's clear that functions are very important
+* and they rule most of javascript structures.
+*/
