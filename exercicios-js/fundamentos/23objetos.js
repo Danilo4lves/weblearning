a937b6f27add7df34ee32f68e1905a9900c97bd7
Danilo Alves <danilovieiraalves@hotmail.com>
2019-04-12 08:30:50 -0300

    Object notes

---
+++ exercicios-js/fundamentos/23objetos.js
@@ -0,0 +1,16 @@
+const obj1 = {}
+obj1.nome = "Danilo Alves"
+obj1.idade = 18
+obj1["universidade"] = "UFBA"
+console.log(obj1)
+
+obj1.nome = "Teste"
+obj1.idade = 30
+obj1["universidade"]="unknown"
+console.log(obj1)
+
+const obj2 = {
+    nome: "test1",
+    idade: 19
+}
+console.log(obj2)
