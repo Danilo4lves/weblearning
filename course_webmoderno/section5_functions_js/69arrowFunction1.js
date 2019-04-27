ae0f77265f792fcccb23cc5e631354ededf2710a
Danilo Alves <danilovieiraalves@hotmail.com>
2019-04-27 15:42:38 -0300

    Arrow function

---
+++ course_webmoderno/section5_functions_js/69arrowFunction1.js
@@ -0,0 +1,13 @@
+let double = function(a) {
+    return 2 * a
+}
+
+double = (a) => {
+    return 2 * a
+}
+
+double = a => 2 * a // Implicity return
+console.log(double(4))
+
+let hello = () => "Hello"
+console.log(hello())
