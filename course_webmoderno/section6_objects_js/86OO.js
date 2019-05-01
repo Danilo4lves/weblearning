ec566e6289405f15dea72ce58998ff514ce44567
Danilo Alves <danilovieiraalves@hotmail.com>
2019-05-01 20:43:25 -0300

    Objects section notes

---
+++ course_webmoderno/section6_objects_js/86OO.js
@@ -0,0 +1,24 @@
+// Non-executable code
+
+// Procedural programming
+processing(value1, value2, value3)
+
+// Oriented Object
+object = {
+    value1,
+    value2,
+    value3,
+    processing() {
+        // ...
+    }
+}
+
+object.processing() // The focus is now the object
+
+/* 
+* Important principles about OO:
+** Abstraction
+** Encapsulation
+** Inheritance (prototype)
+** Polymorfism
+*/
